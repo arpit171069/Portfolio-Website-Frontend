4c51d1bbbf41fc0e61d3c88208c834b3f1ebb39f
Arpit Verma <arpit.verma@postgrad.manchester.ac.uk>
2022-03-11 14:44:21 +0000

    Initial Commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,15 +3,16 @@ import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 let prod = process.env.NODE_ENV == "production";
 
 let csp = ``;
-csp += `base-uri 'self' vitals.vercel-insights.com;`;
-csp += `form-action 'self' vitals.vercel-insights.com;`;
-csp += `default-src 'self' vitals.vercel-insights.com;`;
-csp += `script-src 'self' ${prod ? "" : "'unsafe-eval'"} vitals.vercel-insights.com;`;
-csp += `style-src 'self' https://fonts.googleapis.com 'unsafe-inline' vitals.vercel-insights.com data:;`;
-csp += `img-src 'self' vitals.vercel-insights.com data: blob:;`;
-csp += `font-src 'self' vitals.vercel-insights.com https://fonts.gstatic.com;`;
+csp += `base-uri 'self';`;
+csp += `form-action 'self';`;
+csp += `default-src 'self';`;
+csp += `script-src 'self' ${prod ? "" : "'unsafe-eval'"};`;
+csp += `style-src 'self' https://fonts.googleapis.com 'unsafe-inline' data:;`;
+csp += `img-src 'self' data: blob:;`;
+csp += `font-src 'self' https://fonts.gstatic.com;`;
 csp += `frame-src *;`;
 csp += `media-src *;`;
+csp += `connect-src https://vitals.vercel-insights.com;`;
 
 let referrer = "strict-origin";
 
